0a283b66eaf1eb7dac5dfe483f030d08e2e3d82b
y0c <wonder789@naver.com>
2018-07-12 21:48:47 +0900

    feat(db): Initializing DB connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
+import db from 'db';
+import config from 'config';
 
 const app = new Koa();
 
+db.connect();
+
 app.use(koaBody());
 
+app.use(ctx => ctx.body = config.jwtSecret);
 
 export default app;
